d452374c2fcd3c2f2a6cfc71dee9a75d271a3829
Niputi <7137178+Niputi@users.noreply.github.com>
2022-02-20 20:15:52 +0100

    esm build

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import "raf/polyfill";
 import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 Enzyme.configure({ adapter: new Adapter() });
